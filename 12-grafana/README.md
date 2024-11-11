e3e5d49988b11f5b74ac69a1579a3595939140dd
ImanJowkar <77024269+ImanJowkar@users.noreply.github.com>
2024-11-11 21:36:40 +0330

    updat

--- 12-grafana/README.md
+++ 12-grafana/README.md
@@ -5,6 +5,6 @@
 # generate public and private key for grafana
 openssl req -new -newkey rsa:2048 -days 30 -nodes -x509 -keyout ./grafana/file.key -out ./grafana/file.crt
 
-chmod go+r file.key
+chmod go+r grafana/file.key
 
 ```
--- 3-items/1-ssl-check/check-ssl.py
+++ 3-items/1-ssl-check/check-ssl.py
@@ -3,28 +3,48 @@ import ssl
 from datetime import datetime
 import sys
 
-def check_ssl_expiration(domain, port=443):
+def check_ssl_expiration(domain, port=443, verify_ssl=True):
+
     context = ssl.create_default_context()
-    with socket.create_connection((domain, port)) as sock:
-        with context.wrap_socket(sock, server_hostname=domain) as ssock:
-            cert = ssock.getpeercert()
+
+    if not verify_ssl:
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+
+    try:
+
+        with socket.create_connection((domain, port)) as sock:
+            with context.wrap_socket(sock, server_hostname=domain) as ssock:
+                cert = ssock.getpeercert()
 
 
-    expires = datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
-    remaining_days = (expires - datetime.utcnow()).days
+        if 'notAfter' not in cert:
+            raise ValueError("Certificate does not contain an expiration date ('notAfter')")
 
-    return expires, remaining_days
+
+        expires = datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
+        remaining_days = (expires - datetime.utcnow()).days
+
+        return expires, remaining_days
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        print("Usage: python check_ssl_expiration.py <domain>")
+        print("Usage: python check_ssl_expiration.py <domain_or_ip> [<port>] [--ignore-self-signed]")
         sys.exit(1)
 
+
     domain = sys.argv[1]
+    port = int(sys.argv[2]) if len(sys.argv) > 2 and sys.argv[2].isdigit() else 443  
+    verify_ssl = "--ignore-self-signed" not in sys.argv 
+
 
     try:
-        expiration_date, days_left = check_ssl_expiration(domain)
+        expiration_date, days_left = check_ssl_expiration(domain, port, verify_ssl)
         print(days_left)
     except Exception as e:
         print(f"Error: {e}")
